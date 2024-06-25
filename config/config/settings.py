c7440ccc776bbbb6cc1dafb0020545f0b50934ab
TsubaAnton <antontsuba2003@gmail.com>
2024-06-25 16:51:01 +0200

    homework1.1

--- config/config/settings.py
+++ config/config/settings.py
@@ -183,3 +183,13 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': timedelta(minutes=10),
     },
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',  # Убедитесь в правильности порта и номере базы данных
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
--- config/config/wsgi.py
+++ config/config/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import eventlet
 from django.core.wsgi import get_wsgi_application
 
+# Патчим eventlet до импорта Django для корректной работы с асинхронными операциями
+eventlet.monkey_patch()
+
+# Устанавливаем переменную окружения DJANGO_SETTINGS_MODULE
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Получаем WSGI-приложение Django
 application = get_wsgi_application()
+
